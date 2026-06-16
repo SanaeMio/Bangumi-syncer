5776c73a3093933b62b2d25f8e998601c2a401f9
SanaeMio <i@sanaemio.com>
2026-06-16 16:59:21 +0800

    perf: 优化windows启动脚本

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --no-access-log
-pause 
+py -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --no-access-log
+if errorlevel 1 pause 
